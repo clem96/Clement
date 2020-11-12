e2233b8ba0efe593389a68adf2b92e15b2dbda6f
clem96 <71269491+clem96@users.noreply.github.com>
2020-11-12 14:43:57 +0100

    12/11

---
+++ webPHP/cookie php/cookie/footer.php
@@ -0,0 +1,10 @@
+<footer>
+            <div class="logo"></div>
+            <div class="headTitre">
+                <p>Le site pour les amoureux des cookies</p>
+            </div>
+            <div class="Footlogo"><img class="footlogo" src="cookie.png" alt="image de cookie"></div>
+        </footer>
+    </div>
+
+</body>
