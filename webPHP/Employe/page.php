2fc09132919764ade88f372ce7be79cba3b5b933
clem96 <dubrunfautclem@outlook.fr>
2020-11-16 17:33:46 +0100

    16/11

---
+++ webPHP/Employe/page.php
@@ -0,0 +1,26 @@
+<?php
+require 'test.php';
+require 'Employe.Class.php';
+
+
+'<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  
+ <div class="case">
+
+ 
+     ' .$e[$i]->getNom();
+      $e[$i]->getPrenom();
+      $e[$i]->getAgence();
+      $e[$i]->get_idemploye().';
+</div>
+
+</body>
+</html>';
+
