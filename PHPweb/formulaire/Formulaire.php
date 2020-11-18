186d66edfb05d9f4ecda6b7963ee756a6ef6751b
clem96 <dubrunfautclem@outlook.fr>
2020-11-18 17:15:10 +0100

    18/11/2020 17:15:09,98

---
+++ PHPweb/formulaire/Formulaire.php
@@ -0,0 +1,19 @@
+<?php
+
+echo'<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="post" href="page.php">
+        <input type="text">
+        <input placeholder="Mot de passe" type="password">
+        <input type="submit">
+    </form>
+</body>
+</html>';
